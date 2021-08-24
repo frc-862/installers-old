f521b6e65e919d7c1bcdd207f1cdecef7f38bf11
Fritz Geib <fritz@standland.org>
2021-08-24 10:27:03 -0400

    minor printf fixes

--- 862Installer-debian.sh
+++ 862Installer-debian.sh
@@ -2,18 +2,18 @@
 has() { type -p "$1" &> /dev/null; }
 
 if has sudo ; then
-    printf "Using sudo for root priveleges"
+    printf "Using sudo for root priveleges\n"
 else
-    printf "sudo not found, if you're using doas or other ill just trust that you know what you're doing."
+    printf "sudo not found, if you're using doas or other ill just trust that you know what you're doing.\n"
     exit
 fi
 
 if has apt ; then
-    printf "existing apt installation detected. Updating now."
+    printf "existing apt installation detected. Updating now.\n"
     sudo apt update
     sudo apt upgrade
 else
-    printf "Apt not found in PATH. Please use the apporopriate installed for your distro or fix your PATH"
+    printf "Apt not found in PATH. Please use the apporopriate installed for your distro or fix your PATH\n"
     exit
 fi
 
@@ -24,25 +24,25 @@ sudo apt install vscode.deb
 rm vscode.deb
 
 if has code ; then
-    printf "installing vscode extensions..."
+    printf "installing vscode extensions...\n"
     code --install-extension vscjava.vscode-java-pack
     code --install-extension wpilibsuite.vscode-wpilib
 else
-    printf "ERROR: vscode not detected in PATH."
-    printf "It may have failed to install or failed to add itself to your PATH"
-    printf "You may be able to fix thie by manually install vscode or manually adding /usr/bin/code to your PATH"
+    printf "ERROR: vscode not detected in PATH.\n"
+    printf "It may have failed to install or failed to add itself to your PATH\n"
+    printf "You may be able to fix thie by manually install vscode or manually adding /usr/bin/code to your PATH\n"
 fi
 
-printf "Cloning lightning source code over https into $HOME/Documents/"
-printf "Note: you will need to clone over ssh in order to contribute code"
+printf 'Cloning lightning source code over https into %s/Documents/\n' "$HOME"
+printf "Note: you will need to clone over ssh in order to contribute code\n"
 if [ -d "$HOME/Documents/lightning" ] ; then
-    printf "lightning already appears to be cloned. Pulling latest version now."
+    printf "lightning already appears to be cloned. Pulling latest version now.\n"
     git -C "$HOME/Documents/lightning" pull
 else
-    printf "lightning doesn't appear to be cloned. Cloning now."
-    git clone "https://github.com/frc-862/lightning.git" "$HOME/Documents/lightning"
+    printf "lightning doesn't appear to be cloned. Cloning now.\n"
+    git clone "https://github.com/frc-862/lightning.git" "$HOME/Documents/lightning\n"
 fi
 
-printf "Building gradle..."
+printf "Building gradle...\n"
 "$HOME/Documents/lightning/gradlew" -p "$HOME/Documents/lightning" build
 
--- 862Installer-mac.sh
+++ 862Installer-mac.sh
@@ -1,57 +1,57 @@
 #!/bin/bash
 has() { type -p "$1" &> /dev/null; }
 if has brew ; then
-    printf "existing brew installation detected. Updating now."
+    printf "existing brew installation detected. Updating now.\n"
     brew update
     brew upgrade
 else
-    printf "no brew installation detected. Installing brew now."
+    printf "no brew installation detected. Installing brew now.\n"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 if [ -d "$(brew --prefix)/Cellar/git" ] ; then
-    printf "git is installed. Upgrading now."
+    printf "git is installed. Upgrading now.\n"
     brew upgrade git
 else
-    printf "git doesn't appear to be installed. Installing now."
+    printf "git doesn't appear to be installed. Installing now.\n"
     brew install git
 fi
 
 if [ -d "$(brew --prefix)/Cellar/openjdk@11" ] ; then
-    printf "openjdk11 is installed. Upgrading now."
+    printf "openjdk11 is installed. Upgrading now.\n"
     brew upgrade openjdk@11
 else
-    printf "openjdk11 doesn't appear to be installed. Installing now."
+    printf "openjdk11 doesn't appear to be installed. Installing now.\n"
     brew install openjdk@11
 fi
 
 if [ -d "$(brew --prefix)/Caskroom/visual-studio-code" ] ; then
-    printf "vs code is installed. Upgrading now."
+    printf "vs code is installed. Upgrading now.\n"
     brew upgrade visual-studio-code
 else
-    printf "vs code doesn't appear to be installed. Installing now."
+    printf "vs code doesn't appear to be installed. Installing now.\n"
     brew install visual-stuio-code
 fi
 
 
 if has code ; then
-    printf "installing vscode extensions..."
+    printf "installing vscode extensions...\n"
     code --install-extension vscjava.vscode-java-pack
     code --install-extension wpilibsuite.vscode-wpilib
 else
-    printf "ERROR: vscode failed to install"
+    printf "ERROR: vscode failed to install\n"
 fi
 
-printf "Cloning lightning source code over https into $HOME/Documents/"
-printf "Note: you will need to clone over ssh in order to contribute code"
+printf 'Cloning lightning source code over https into %s/Documents/\n' "$HOME"
+printf "Note: you will need to clone over ssh in order to contribute code\n"
 if [ -d "$HOME/Documents/lightning" ] ; then
-    printf "lightning already appears to be cloned. Pulling latest version now."
+    printf "lightning already appears to be cloned. Pulling latest version now.\n"
     git -C "$HOME/Documents/lightning" pull
 else
-    printf "lightning doesn't appear to be cloned. Cloning now."
+    printf "lightning doesn't appear to be cloned. Cloning now.\n"
     git clone "https://github.com/frc-862/lightning.git" "$HOME/Documents/lightning"
 fi
 
-printf "Building gradle..."
+printf "Building gradle...\n"
 "$HOME/Documents/lightning/gradlew" -p "$HOME/Documents/lightning" build
 
