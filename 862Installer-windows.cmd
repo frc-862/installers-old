9270c09322b1b660771b315522c6d6c69fadc1a4
Fritz Geib <fritz@standland.org>
2021-09-13 13:53:46 -0400

    fix variable declaring

--- 862Installer-windows.cmd
+++ 862Installer-windows.cmd
@@ -4,7 +4,7 @@ if NOT exist %UserProfile%\scoop\shims\scoop.cmd (
     ::if scoop is not installed, install it
     echo Scoop Not found. Installing now.
     powershell -ExecutionPolicy Unrestricted -Command "iwr -useb get.scoop.sh | iex"
-    set PATH=%PATH%;%UserProfile%\scoop\shims
+    set PATH "%PATH%;%UserProfile%\scoop\shims"
 )
 
 ::define functions for each package manager
@@ -13,24 +13,21 @@ if NOT exist %UserProfile%\scoop\shims\scoop.cmd (
 :update
     call scoop install git
     call scoop update
-    exit /b
 
 ::installreqs: install required packages
 :installreqs
     call scoop install git
     call scoop bucket add java
     call scoop install openjdk11
-    set PATH=%path%;%UserProfile%\scoop\apps\openjdk11\current\bin
-    exit /b
+    set PATH "%path%;%UserProfile%\scoop\apps\openjdk11\current\bin"
 
 ::installopts: install optional packages
 :installopts
     call scoop bucket add extras
     call scoop install vscode lazygit
-    exit /b
 
 ::pkgmanager: the name of the detected package manager
-set pkgmanager=scoop
+set pkgmanager "scoop"
 
 ::run the defined update, installreqs, and installopts functions
 call update
