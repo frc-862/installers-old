c36a3cddce8144add96eec884eb586243a69bdc0
Fritz Geib <fritz@standland.org>
2021-10-18 13:48:52 -0400

    PROG-27 fix some code that I copy-pasted

--- Uninstaller-bash.sh
+++ Uninstaller-bash.sh
@@ -34,7 +34,7 @@ elif has apt ; then
     uninstall() { $rootstring apt -y purge git openjdk-11-jdk wget software-properties-common code; }
     pkgmanager="apt"
 elif has pacman ; then
-    installreqs() { $rootstring pacman --noconfirm -R git jdk11-openjdk code lazygit; }
+    uninstall() { $rootstring pacman --noconfirm -R git jdk11-openjdk code lazygit; }
     pkgmanager="pacman"
 elif has brew ; then
     uninstall() { ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"; }
@@ -49,7 +49,7 @@ fi
 
 #run the defined update, installreqs, and installopts functions
 printf '\033[32m%s installation detected\nuninstalling all packages from %s...\n\033[39m' "$pkgmanager" "$pkgmanager"
-update
+uninstall
 exitcode=$?
 if [ $exitcode -eq 0 ] ; then
     printf "\033[32muninstall completed successfully\n\033[39m"
