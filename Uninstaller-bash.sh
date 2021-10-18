a1fd1cda3d993b76a8a9e3fad29e05322210c9d5
Fritz Geib <fritz@standland.org>
2021-10-18 13:39:18 -0400

    PROG-27 add has function and rootstring detection to uninstaller

--- Uninstaller-bash.sh
+++ Uninstaller-bash.sh
@@ -1,4 +1,32 @@
 #!/bin/bash
+
+#Define functions
+#Has: check if a program is in PATH
+has() { type -p "$1" &> /dev/null; }
+
+#Define constants
+os=$(uname -s)
+
+#detect a program to use to obtain root privileges
+#set rootstring variable to say what command to use
+if [ "$os" == "Darwin" ] ; then
+    printf "\033[32mno root privileges needed on macos\n\033[39m"
+elif has sudo ; then
+    rootstring="sudo"
+    printf '\033[32musing sudo (%s) for root privileges\n\033[39m' "$(type -p $rootstring)"
+elif has doas ; then
+    rootstring="doas"
+    printf '\033[32musing doas (%s) for root privileges\n\033[39m' "$(type -p $rootstring)"
+elif [ "$EUID" -eq 0 ] ; then
+    rootstring=""
+    printf '\033[32musing current user (%s) for root privileges\n\033[39m' "$(whoami)"
+else
+    printf "\033[31merror: no root privileges\n\033[39m"
+    printf "\033[31mtry running this script as root or verifying that sudo or doas is installed and in your PATH\n\033[39m"
+    exit 1
+fi
+
+#detect a compatible package manager to uninstall packages
 if has apt && [ -f "/etc/ubuntu-release" ] ; then
     uninstall() { $rootstring apt -y purge git openjdk-11-jdk wget software-properties-common code lazygit; }
     pkgmanager="apt (ubuntu)"
