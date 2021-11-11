662da9a5c5f1f214998211d02a37932f15e97fb0
Fritz Geib <fritz@standland.org>
2021-11-11 07:08:57 -0500

    PROG-52 add comments to powershell installer

--- powershellInstaller.ps1
+++ powershellInstaller.ps1
@@ -1,10 +1,14 @@
+#check if scoop is installed
 if ( Test-Path "$HOME\scoop\shims\scoop" ) {
+    #if it is, continue
     Write-Output "Existing Scoop installation found."
 } else {
     Write-Output "Scoop Not found. Installing now."
+    #if it isn't, get the install script from the scoop website
     Invoke-WebRequest -useb "https://get.scoop.sh" | Invoke-Expression
 }
 
+#install software from scoop
 Write-Output "Installing git..."
 scoop install git
 Write-Output "Updating Scoop..."
@@ -15,7 +19,9 @@ scoop bucket add extras
 Write-Output "Installing java and vscode..."
 scoop install openjdk11 vscode
 
+#check if vs code installed correctly
 if ( Test-Path "$HOME\scoop\shims\code" ) {
+    #if it did, install wpilib extension and java extension pack
     Write-Output "Installing vs code extensions"
     code --install-extension vscjava.vscode-java-pack
     code --install-extension wpilibsuite.vscode-wpilib
@@ -26,7 +32,9 @@ if ( Test-Path "$HOME\scoop\shims\code" ) {
 
 Write-Output "Cloning lightning source code over https into $HOME\Documents\lightning"
 Write-Output "Note: you will need to clone over ssh if you want to contribute code"
+#clone lighning into ~/Documents/lighning
 git clone "https://github.com/frc-862/lightning.git" "$HOME\Documents\lightning"
 
+#run a gradle build in the lighning folder
 Write-Output "Building gradle..."
 Invoke-Expression "$HOME\Documents\lightning\gradlew build"
