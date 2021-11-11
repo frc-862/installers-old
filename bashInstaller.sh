5e5e6e1b03f52c79649bffcd9c97496861ded2e2
Fritz Geib <fritz@standland.org>
2021-11-11 14:08:41 -0500

    PROG-51 functions for error messages

--- bashInstaller.sh
+++ bashInstaller.sh
@@ -3,6 +3,10 @@
 #Define functions
 #Has: check if a program is in PATH
 has() { type -p "$1" &> /dev/null; }
+#Error, Warn, ok: print message in red, orange, or green text
+error() { printf "\033[31m$1\n\033[39m"; }
+warn() { printf "\033[33m$1\n\033[39m"; }
+ok() { printf "\033[32m$1\n\033[39m"; }
 
 #Define constants
 os=$(uname -s)
@@ -10,19 +14,19 @@ os=$(uname -s)
 #detect a program to use to obtain root privileges
 #set rootstring variable to say what command to use
 if [ "$os" == "Darwin" ] ; then
-    printf "\033[32mno root privileges needed on macos\n\033[39m"
+    ok "no root privileges needed on macos"
 elif has sudo ; then
     rootstring="sudo"
-    printf '\033[32musing sudo (%s) for root privileges\n\033[39m' "$(type -p $rootstring)"
+    ok "using sudo ($(type -p $rootstring)) for root privileges"
 elif has doas ; then
     rootstring="doas"
-    printf '\033[32musing doas (%s) for root privileges\n\033[39m' "$(type -p $rootstring)"
+    ok "using doas ($(type -p $rootstring)) for root privileges"
 elif [ "$EUID" -eq 0 ] ; then
     rootstring=""
-    printf '\033[32musing current user (%s) for root privileges\n\033[39m' "$USER"
+    ok "using current user ($USER) for root privileges"
 else
-    printf "\033[31merror: no root privileges\n\033[39m"
-    printf "\033[31mtry running this script as root or verifying that sudo or doas is installed and in your PATH\n\033[39m"
+    error "error: no root privileges"
+    error "try running this script as root or verifying that sudo or doas is installed and in your PATH"
     exit 1
 fi
 
@@ -31,7 +35,7 @@ if [ "$os" == "Darwin" ] ; then
     # only install brew from scratch on mac
     if ! has brew ; then
         #if brew isn't installed and we are on mac, then install it
-        printf "\033[32mno brew installation detected\ninstalling brew...\n\033[39m"
+        ok "no brew installation detected\ninstalling brew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
     #define functions for each package manager
@@ -115,40 +119,40 @@ elif has scoop ; then
     }
     pkgmanager="scoop"
 else
-    printf "\033[31mno supported package manager found\ntry verifying that one is installed and in your PATH\n\033[39m"
+    error "no supported package manager found\ntry verifying that one is installed and in your PATH"
     exit 1
 fi
 
 #run the defined update, installreqs, and installopts functions
-printf '\033[32m%s installation detected\nupgrading %s...\n\033[39m' "$pkgmanager" "$pkgmanager"
+ok "$pkgmanager installation detected\nupgrading $pkgmanager..."
 update
 updateexitcode=$?
 if [ $updateexitcode -eq 0 ] ; then
-    printf "\033[32mupdate completed successfully\n\033[39m"
+    ok "update completed successfully"
 else
     #don't exit if the update fails
-    printf '\033[33mwarning: update failed with exit code %s\n\033[39m' "$updateexitcode"
+    warn "warning: update failed with exit code $updateexitcode"
 fi
 
-printf "\033[32minstalling required packages...\n\033[39m"
+ok "installing required packages..."
 installreqs
 installexitcode=$?
 if [ $installexitcode -eq 0 ] ; then
-    printf "\033[32minstallreqs completed successfully\n\033[39m"
+    ok "installreqs completed successfully"
 else
     #exit if a non-0 exit code is recieved
-    printf '\033[31merror: installreqs failed with exit code %s\nplease open an issue on jira for assistance\n\033[39m' "$installexitcode"
+    error "error: installreqs failed with exit code $installexitcode\nplease open an issue on jira for assistance"
     exit $installexitcode
 fi
 
-printf "\033[32minstalling optional packages...\n\033[39m"
+ok "installing optional packages..."
 installopts
 installexitcode=$?
 if [ $installexitcode -eq 0 ] ; then
-    printf "\033[32minstallopts completed successfully\n\033[39m"
+    ok "installopts completed successfully"
 else
     #don't exit if installopts fails, as the build can still work
-    printf '\033[33mwarning: installopts failed with exit code %s\n\033[39m' "$installexitcode"
+    warn "warning: installopts failed with exit code $installexitcode"
 fi
 
 #install vscode extensions
@@ -163,38 +167,38 @@ else
 fi
 
 #build section
-printf '\033[32mcloning lightning source code into %s/Documents/\n\033[39m' "$HOME"
-printf "\033[33mnote: you will need to clone over ssh in order to contribute code\n\033[39m"
+ok "cloning lightning source code into $HOME/Documents/"
+warn "note: you will need to clone over ssh in order to contribute code"
 #clone lightning repo
 if [ -d "$HOME/Documents/lightning" ] ; then
-    printf "\033[32mlightning code detected\npulling latest version...\n\033[39m"
+    ok "lightning code detected\npulling latest version..."
     git -C "$HOME/Documents/lightning" pull
     gitexitcode=$?
     if [ $gitexitcode -eq 0 ] ; then
-        printf "\033[32mpull completed successfully\n\033[39m"
+        ok "pull completed successfully"
     else
-        printf '\033[31merror: pull failed with exit code %s\nplease open an issue on jira for assistance\n\033[39m' "$gitexitcode"
+        error "error: pull failed with exit code $gitexitcode\nplease open an issue on jira for assistance"
         exit $gitexitcode
     fi
 else
-    printf "\033[32mno lightning code detected\ncloning new code...\n\033[39m"
+    ok "no lightning code detected\ncloning new code..."
     git clone "https://github.com/frc-862/lightning.git" "$HOME/Documents/lightning"
     gitexitcode=$?
     if [ $gitexitcode -eq 0 ] ; then
-        printf "\033[32mpull completed successfully\n\033[39m"
+        ok "pull completed successfully"
     else
-        printf '\033[31merror: clone failed with exit code %s\nplease open an issue on jira for assistance\n\033[39m' "$gitexitcode"
+        error "error: clone failed with exit code $gitexitcode\nplease open an issue on jira for assistance"
         exit $gitexitcode
     fi
 fi
 
 #build lightning repo
-printf "\033[32mbuilding gradle...\n\033[39m"
+ok "building gradle..."
 "$HOME/Documents/lightning/gradlew" -p "$HOME/Documents/lightning" build
 buildstatus=$?
 #detect if the build failed based on its exit code
 if [ $buildstatus -eq 0 ] ; then
-    printf "\033[32mbuild completed successfully\n\033[39m"
+    ok "build completed successfully"
 else
-    printf '\033[31merror: build failed with exit code %s\nplease open an issue on jira for assistance\n\033[39m' "$buildstatus"
+    error "error: build failed with exit code $buildstatus\nplease open an issue on jira for assistance"
 fi
