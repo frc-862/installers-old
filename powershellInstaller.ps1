dbb28ae79422e84043789bcf5966e4a0187db870
Fritz Geib <fritz@standland.org>
2021-11-29 12:30:16 -0500

    PROG-52 clean up a bit and add some better docs

--- powershellInstaller.ps1
+++ powershellInstaller.ps1
@@ -1,6 +1,7 @@
-$wpilibVersion="2021.3.1"
-$wpilibType="Windows64"
-$wpilibExtension="iso"
+#define constants
+$WPILIB_VERSION="2021.3.1"
+$WPILIB_TYPE="Windows64"
+$WPILIB_EXTENSION="iso"
 
 #check if scoop is installed
 if ( Test-Path "$HOME\scoop\shims\scoop" ) {
@@ -12,27 +13,30 @@ if ( Test-Path "$HOME\scoop\shims\scoop" ) {
     Invoke-WebRequest -useb "https://get.scoop.sh" | Invoke-Expression
 }
 
-#install software from scoop
+#install git in order to allow updating scoop
 Write-Output "Installing git..."
 scoop install git
+#update scoop
 Write-Output "Updating Scoop..."
 scoop update
+#add extras bucket for lazygit
 Write-Output "Installing scoop buckets..."
 scoop bucket add extras
 Write-Output "Installing lazygit, wget, and 7zip..."
 scoop install lazygit
-scoop install wget
-scoop install 7zip
+scoop install wget #install wget to download wget
+scoop install 7zip #install 7zip to extract wpilib iso
 
-$wpilibUrl="https://github.com/wpilibsuite/allwpilib/releases/download/v$wpilibVersion/WPILib_$wpilibType-$wpilibVersion.$wpilibExtension"
-$wpilibFilename="WPILib_$wpilibType-$wpilibVersion.$wpilibExtension"
+Write-Output "Downloading wpilib installer..."
+$wpilibUrl="https://github.com/wpilibsuite/allwpilib/releases/download/v$WPILIB_VERSION/WPILib_$WPILIB_TYPE-$WPILIB_VERSION.$WPILIB_EXTENSION"
+$wpilibFilename="WPILib_$WPILIB_TYPE-$WPILIB_VERSION.$WPILIB_EXTENSION"
 wget "$wpilibUrl"
 
 Write-Output "Extracting wpilib installer..."
-7z x -y -o".\$wpilibType" ".\$wpilibFilename"
+7z x -y -o".\$WPILIB_TYPE" ".\$wpilibFilename"
 
 Write-Output "Running wpilib installer"
-".\$wpilibType/WPILibInstaller.exe"
+".\$WPILIB_TYPE/WPILibInstaller.exe"
 
 Write-Output "Cloning lightning source code over https into $HOME\Documents\lightning"
 Write-Output "Note: you will need to clone over ssh if you want to contribute code"
