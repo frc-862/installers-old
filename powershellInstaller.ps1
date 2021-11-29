102a2e58868d1d30b3cc98d92cc5729ab45ea33b
Fritz Geib <fritz@standland.org>
2021-11-29 14:40:03 -0500

    PROG-53 check if wpilib is downloaded

--- powershellInstaller.ps1
+++ powershellInstaller.ps1
@@ -2,6 +2,8 @@
 $WPILIB_VERSION="2021.3.1"
 $WPILIB_TYPE="Windows64"
 $WPILIB_EXTENSION="iso"
+$wpilibUrl="https://github.com/wpilibsuite/allwpilib/releases/download/v$WPILIB_VERSION/WPILib_$WPILIB_TYPE-$WPILIB_VERSION.$WPILIB_EXTENSION"
+$wpilibFilename="WPILib_$WPILIB_TYPE-$WPILIB_VERSION.$WPILIB_EXTENSION"
 
 #check if scoop is installed
 if ( Test-Path "$HOME\scoop\shims\scoop" ) {
@@ -27,13 +29,15 @@ scoop install lazygit
 scoop install aria2 #install aria2 to download wget
 scoop install 7zip #install 7zip to extract wpilib iso
 
-Write-Output "Downloading wpilib installer..."
-$wpilibUrl="https://github.com/wpilibsuite/allwpilib/releases/download/v$WPILIB_VERSION/WPILib_$WPILIB_TYPE-$WPILIB_VERSION.$WPILIB_EXTENSION"
-$wpilibFilename="WPILib_$WPILIB_TYPE-$WPILIB_VERSION.$WPILIB_EXTENSION"
-aria2c "$wpilibUrl"
+if ( -not (Test-Path ".\$wpilibFilename") ) {
+    Write-Output "Downloading wpilib installer..."
+    aria2c "$wpilibUrl"
+}
 
-Write-Output "Extracting wpilib installer..."
-7z x -y -o".\$WPILIB_TYPE" ".\$wpilibFilename"
+if ( -not (Test-Path ".\$WPILIB_TYPE") ) {
+    Write-Output "Extracting wpilib installer..."
+    7z x -y -o".\$WPILIB_TYPE" ".\$wpilibFilename"
+}
 
 Write-Output "Running wpilib installer"
 Invoke-Expression ".\$WPILIB_TYPE\WPILibInstaller.exe"
