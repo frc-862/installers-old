75a9f16f5197e495930c5f337e2a9c8e73b00862
Fritz Geib <fritz@standland.org>
2021-11-29 20:07:01 -0500

    PROG-52 PROG-63 lots more comments, more readable code, and add basic ssh check

--- bashInstaller.sh
+++ bashInstaller.sh
@@ -5,207 +5,250 @@
 has() { type -p "$1" &> /dev/null; }
 #Error, Warn, ok: print message in red, orange, or green text
 #use of a variable in printf fstring is intentional here
-error() { printf "\033[31m$1\n\033[39m"; }
-warn() { printf "\033[33m$1\n\033[39m"; }
-ok() { printf "\033[32m$1\n\033[39m"; }
+error() { printf "\033[31mERROR: $1\n\033[39m"; }
+warn() { printf "\033[33mWARNING: $1\n\033[39m"; }
+ok() { printf "\033[32mOK: $1\n\033[39m"; }
 
 #Define constants
-os="$(uname -s)"
-wpilibVersion="2021.3.1"
+OS="$(uname -s)"
+WPILIB_VERSION="2021.3.1"
 
-#detect a program to use to obtain root privileges
-#set rootstring variable to say what command to use
-if [ "$os" == "Darwin" ] ; then
-    ok "no root privileges needed on macos"
+#detect a program to use for root privileges
+# andset ROOT_STRING variable to the found command
+if [ "$OS" == "Darwin" ] ; then
+    ok "no root privileges needed on macOS"
 elif has sudo ; then
-    rootstring="sudo"
-    ok "using sudo ($(type -p $rootstring)) for root privileges"
+    ROOT_STRING="sudo"
+    ok "using sudo ($(type -p $ROOT_STRING)) for root privileges"
 elif has doas ; then
-    rootstring="doas"
-    ok "using doas ($(type -p $rootstring)) for root privileges"
+    ROOT_STRING="doas"
+    ok "using doas ($(type -p $ROOT_STRING)) for root privileges"
 elif [ "$EUID" -eq 0 ] ; then
-    rootstring=""
+    ROOT_STRING=""
     ok "using current user ($USER) for root privileges"
 else
-    error "error: no root privileges"
-    error "try running this script as root or verifying that sudo or doas is installed and in your PATH"
+    error "no root privileges\ntry running this script as root and verifying that sudo/doas is installed and in your PATH"
     exit 1
 fi
 
-#detect which wpilib release to download
-if [ "$os" == "Linux" ] ; then
-    wpilibType="Linux"
-    wpilibExtension="tar.gz"
-elif [ "$os" == "Darwin" ] ; then
-    wpilibType="macOS"
-    wpilibExtension="dmg"
-elif [[ "$os" == *"MINGW64"* ]] ; then
-    wpilibType="Windows64"
-    wpilibExtension="iso"
-elif [[ "$os" == *"MINGW"* ]] ; then
-    wpilibType="Windows32"
-    wpilibExtension="iso"
-fi
+#detect which wpilib release to download based on the info from uname -s
+case $OS in
+
+    "Linux")
+        WPILIB_TYPE="Linux"
+        WPILIB_EXTENSION="tar.gz" ;;
+
+    "Darwin")
+        WPILIB_TYPE="macOS"
+        WPILIB_EXTENSION="dmg" ;;
+
+    *"MINGW64"*)
+        WPILIB_TYPE="Windows64"
+        WPILIB_EXTENSION="iso" ;;
+
+    *"MINGW"*)
+        WPILIB_TYPE="Windows32"
+        WPILIB_EXTENSION="iso" ;;
+
+esac
+
+#define constants for download url and filename to save to
+WPILIB_URL="https://github.com/wpilibsuite/allwpilib/releases/download/v$WPILIB_VERSION/WPILib_$WPILIB_TYPE-$WPILIB_VERSION.$WPILIB_EXTENSION"
+WPILIB_FILENAME="WPILib_$WPILIB_TYPE-$WPILIB_VERSION.$WPILIB_EXTENSION"
 
 #detect a compatible package manager to install packages
-if [ "$os" == "Darwin" ] ; then
+if [ "$OS" == "Darwin" ] ; then
     # only install brew from scratch on mac
     if ! has brew ; then
-        #if brew isn't installed and we are on mac, then install it
         ok "no brew installation detected\ninstalling brew..."
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
     #define functions for each package manager
-    #these functions apply to all other package managers also
-    #update: get the latest version of all packages
+    #these functions are also defined for all other package managers
+    #update: get the latest version of all installed packages
     update() {
         brew update;
         brew upgrade;
     }
-    #installreqs: install required packages
-    installreqs() {
+
+    #installReqs: install required packages
+    installReqs() {
         brew install git wget;
     }
-    #installopts: install optional packages
-    installopts() {
+
+    #installOpts: install optional packages
+    installOpts() {
         brew install lazygit;
     }
-    #pkgmanager: the name of the detected package manager
-    pkgmanager="brew"
+
+    #PKG_MANAGER: the name of the detected package manager
+    PKG_MANAGER="brew"
+
 elif has apt ; then
+
     update() {
-        $rootstring apt update;
-        $rootstring apt -y upgrade;
+        $ROOT_STRING apt update;
+        $ROOT_STRING apt -y upgrade;
     }
-    installreqs() {
-        $rootstring apt -y install git wget tar;
+
+    installReqs() {
+        $ROOT_STRING apt -y install git wget tar;
     }
-    if [ -f "/etc/ubuntu-release" ] ; then # seperate ubuntu and debian installers because of lazygit PPA
-        installopts() {
-            $rootstring apt -y install software-properties-common
-            #Lazygit PPA is Ubuntu only
-            $rootstring add-apt-repository "ppa:lazygit-team/release"
-            $rootstring apt -y update
-            $rootstring apt -y install lazygit
+
+    if [ -f "/etc/ubuntu-release" ] ; then # seperate ubuntu and debian installers because lazygit PPA is ubuntu only
+
+        installOpts() {
+            $ROOT_STRING apt -y install software-properties-common
+            $ROOT_STRING add-apt-repository "ppa:lazygit-team/release"
+            $ROOT_STRING apt -y update
+            $ROOT_STRING apt -y install lazygit
         }
-        pkgmanager="apt (ubuntu)"
+        PKG_MANAGER="apt (ubuntu)"
     else
-        installopts() { true; }
-        pkgmanager="apt"
+        installOpts() { true; } #no optional packages on debian
+        PKG_MANAGER="apt"
     fi
+
 elif has pacman ; then
-    update() { $rootstring pacman --noconfirm -Syu; }
-    installreqs() { $rootstring pacman --noconfirm -S git wget tar; }
-    installopts() { $rootstring pacman --noconfirm -S lazygit; }
-    pkgmanager="pacman"
+
+    update() {
+        $ROOT_STRING pacman --noconfirm -Syu;
+    }
+
+    installReqs() {
+        $ROOT_STRING pacman --noconfirm -S git wget tar;
+    }
+
+    installOpts() {
+        $ROOT_STRING pacman --noconfirm -S lazygit;
+    }
+
+    PKG_MANAGER="pacman"
+
 elif has scoop ; then
+
     update() {
         scoop install git;
         scoop update;
     }
-    installreqs() { scoop install git;
+
+    installReqs() {
+        scoop install git;
         scoop bucket add java;
         scoop install wget;
-        scoop install 7zip;
+        scoop install 7zip; #7zip is required to extract windows iso
     }
-    installopts() {
+
+    installOpts() {
         scoop bucket add extras;
         scoop install lazygit;
     }
-    pkgmanager="scoop"
+
+    PKG_MANAGER="scoop"
+
 else
     error "no supported package manager found\ntry verifying that one is installed and in your PATH"
     exit 1
 fi
 
-#run the defined update, installreqs, and installopts functions
-ok "$pkgmanager installation detected\nupgrading $pkgmanager..."
+#run the defined update, installReqs, and installOpts functions
+ok "$PKG_MANAGER installation detected\nupgrading $PKG_MANAGER..."
 update
-updateexitcode=$?
-if [ $updateexitcode -eq 0 ] ; then
-    ok "update completed successfully"
-else
-    #don't exit if the update fails
-    warn "warning: update failed with exit code $updateexitcode"
-fi
+
+updateExitCode=$?
+case $updateExitCode in
+    0)  ok "update completed successfully";;
+    *)  warn "update failed with exit code $updateExitCode" #don't exit if the update fails
+esac
 
 ok "installing required packages..."
-installreqs
-installexitcode=$?
-if [ $installexitcode -eq 0 ] ; then
-    ok "installreqs completed successfully"
-else
-    #exit if a non-0 exit code is recieved
-    error "error: installreqs failed with exit code $installexitcode\nplease open an issue on jira for assistance"
-    exit $installexitcode
-fi
+installReqs
+
+installExitCode=$?
+case $installExitCode in
+    0)  ok "installReqs completed successfully" ;;
+    *)  error "installReqs failed with exit code $installExitCode\nplease open an issue on jira for assistance"
+        exit $installExitCode ;; #exit if a non-0 exit code is recieved 
+esac
 
 ok "installing optional packages..."
-installopts
-installexitcode=$?
-if [ $installexitcode -eq 0 ] ; then
-    ok "installopts completed successfully"
-else
-    #don't exit if installopts fails, as the build can still work
-    warn "warning: installopts failed with exit code $installexitcode"
-fi
+installOpts
 
-wpilibUrl="https://github.com/wpilibsuite/allwpilib/releases/download/v$wpilibVersion/WPILib_$wpilibType-$wpilibVersion.$wpilibExtension"
-wpilibFilename="WPILib_$wpilibType-$wpilibVersion.$wpilibExtension"
+installExitCode=$?
+case $installExitCode in
+    0)  ok "installOpts completed successfully" ;;
+    *)  warn "installOpts failed with exit code $installExitCode" #don't exit if installOpts fails, as the build can still work
+esac
+
 ok "downloading wpilib installer..."
-wget "$wpilibUrl" -O "$wpilibFilename"
-
-if [ "$wpilibExtension" == "dmg" ] ; then
-    ok "Mounting wpilib installer..."
-    hdiutil attach -readonly "./$wpilibFilename" #dmg needs to be mounted
-    ok "Launching wpilib installer..."
-    /Volumes/WPILibInstaller/WPILibInstaller.app/Contents/MacOS/WPILibInstaller
-    ok "Unmounting wpilib installer..."
-    hdiutil detach /Volumes/WPILibInstaller
-elif [ "$wpilibExtension" == "tar.gz" ] ; then
-    ok "extracting wpilib installer..."
-    tar -xvzf "./$wpilibFilename" #tgz is extractable by tar
-    ok "launching wpilib installer..."
-    "./WPILib_$wpilibType-$wpilibVersion/WPILibInstaller"
-elif [ "$wpilibExtension" == "iso" ] ; then
-    ok "extracting wpilib installer..."
-    7z x -y -o"./$wpilibType" "./$wpilibFilename" #iso can be extracted with 7zip
-    ok "launching wpilib installer..."
-    "./$wpilibType/WPILibInstaller.exe"
+if [ ! -f "./$WPILIB_FILENAME" ] ; then #skip download if file is already downloaded
+    wget "$WPILIB_URL" -O "$WPILIB_FILENAME"
 fi
+
+case $WPILIB_EXTENSION in #different methods for installing and running each archive
+    "dmg")
+        ok "Mounting wpilib installer..."
+        hdiutil attach -readonly "./$WPILIB_FILENAME" #dmg needs to be mounted using hdiutil on mac
+
+        ok "Launching wpilib installer..."
+        /Volumes/WPILibInstaller/WPILibInstaller.app/Contents/MacOS/WPILibInstaller
+
+        ok "Unmounting wpilib installer..."
+        hdiutil detach /Volumes/WPILibInstaller ;;
+    "tar.gz")
+        ok "extracting wpilib installer..."
+        tar -xvzf "./$WPILIB_FILENAME" #.tar.gz can be extracted using tar
+
+        ok "launching wpilib installer..."
+        "./WPILib_$WPILIB_TYPE-$WPILIB_VERSION/WPILibInstaller" ;;
+    "iso")
+        ok "extracting wpilib installer..."
+        7z x -y -o"./$WPILIB_TYPE" "./$WPILIB_FILENAME" #iso can be extracted with 7zip
+
+        ok "launching wpilib installer..."
+        "./$WPILIB_TYPE/WPILibInstaller.exe" ;;
+esac
 
 #clone lightning repo
 ok "cloning lightning source code into $HOME/Documents/"
-warn "note: you will need to clone over ssh in order to contribute code"
+
+#check if lightning is already cloned
 if [ -d "$HOME/Documents/lightning" ] ; then
     ok "lightning code detected\npulling latest version..."
     git -C "$HOME/Documents/lightning" pull
-    gitexitcode=$?
-    if [ $gitexitcode -eq 0 ] ; then
-        ok "pull completed successfully"
-    else
-        error "error: pull failed with exit code $gitexitcode\nplease open an issue on jira for assistance"
-        exit $gitexitcode
-    fi
+
+    gitExitCode=$?
+    case $gitExitCode in
+        0)  ok "pull completed successfully" ;;
+        *)
+            error "pull failed with exit code $gitExitCode\nplease open an issue on jira for assistance"
+            exit $gitExitCode ;;
+    esac
 else
     ok "no lightning code detected\ncloning new code..."
-    git clone "https://github.com/frc-862/lightning.git" "$HOME/Documents/lightning"
-    gitexitcode=$?
-    if [ $gitexitcode -eq 0 ] ; then
-        ok "pull completed successfully"
+    #check if ssh is set up
+    if [ -f "$HOME/.ssh/id_ed25519" ] ; then
+        git clone "git@github.com:frc-862/lightning.git" "$HOME/Documents/lightning"
     else
-        error "error: clone failed with exit code $gitexitcode\nplease open an issue on jira for assistance"
-        exit $gitexitcode
+        git clone "https://github.com/frc-862/lightning.git" "$HOME/Documents/lightning"
+        warn "note: you will need to clone over ssh in order to contribute code"
     fi
+
+    gitExitCode=$?
+    case $gitExitCode in
+        0)  ok "pull completed successfully" ;;
+        *)
+            error "clone failed with exit code $gitExitCode\nplease open an issue on jira for assistance"
+            exit $gitExitCode ;;
+    esac
 fi
 
 #build lightning repo
 ok "building gradle..."
 "$HOME/Documents/lightning/gradlew" -p "$HOME/Documents/lightning" build
-buildstatus=$?
-#detect if the build failed based on its exit code
-if [ $buildstatus -eq 0 ] ; then
-    ok "build completed successfully"
-else
-    error "error: build failed with exit code $buildstatus\nplease open an issue on jira for assistance"
-fi
+
+buildExitCode=$?
+case $buildExitCode in
+    0) ok "build completed successfully" ;;
+    *) error "build failed with exit code $buildExitCode\nplease open an issue on jira for assistance" ;;
+esac
