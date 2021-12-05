78b1f763e6aa88e93caeae8b90d990561b84956b
Fritz Geib <fritz@standland.org>
2021-12-04 20:24:51 -0500

    PROG-74 restructure and compact a bit

--- chocoInstaller.ps1
+++ chocoInstaller.ps1
@@ -1,28 +1,23 @@
-
 #Install Chocolatey
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+Write-Output "Installing Chocolatey..."
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-choco install git lazygit openjdk11 -y
-
-#Install NI game tools (driver station, rio imager, etc.)
-#choco install ni-frcgametools -y
-
-#Install WPLIB
-choco install wpilib -y --params="'/ProgrammingLanguage:java'"#/AllowUserInteraction:true
-
-choco install frc-radioconfigurationutility ctre-phoenixframework -y
-
+#Install specified packages, in order
+Write-Output "Installing packages..."
+choco install -y git lazygit openjdk11 ni-frcgametools wpilib frc-radioconfigurationutility ctre-phoenixframework
 
 refreshenv
 
 Write-Output "Cloning lightning source code over https into $HOME/Documents/lightning"
 Write-Output "Note: you will need to clone over ssh if you want to contribute code"
-#clone lighning into ~/Documents/lighning
+
 if ( Test-Path "$HOME/Documents/lightning" ) {
-    # manually call git because git doesn't get added to path automatically even with refreshenv
+    # manually call git's path because git doesn't get added to path automatically even with refreshenv
     & "C:\Program Files\Git\cmd\git" "-C" "$HOME/Documents/lightning" "pull"
 } else {
+    #clone lighning into ~/Documents/lighning
     & "C:\Program Files\Git\cmd\git" "clone" "https://github.com/frc-862/lightning.git" "$HOME/Documents/lightning"
 }
 
