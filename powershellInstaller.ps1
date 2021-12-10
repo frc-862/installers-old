af622b9bf1666a284dff4b137e7066aa963879db
Fritz Geib <fritz@standland.org>
2021-12-09 22:30:22 -0500

    PROG-82 no cmd directory

--- powershellInstaller.ps1
+++ powershellInstaller.ps1
@@ -9,4 +9,4 @@ Write-Host "Installing git..." -ForegroundColor Green
 choco install -y git
 refreshenv
 
-& "$Env:Programfiles\git\cmd\git-bash.exe" --login -i -c "./bashInstaller.sh"
+& "$Env:Programfiles\git\git-bash.exe" --login -i -c "./bashInstaller.sh"
