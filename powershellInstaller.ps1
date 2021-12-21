8ad9c09542573a6d95593a12001729c831b848fe
Fritz Geib <fritz@standland.org>
2021-12-20 20:42:16 -0500

    PROG-82 standardize colors

--- powershellInstaller.ps1
+++ powershellInstaller.ps1
@@ -1,19 +1,19 @@
 #Check elevation and powershell ver first
 #requires -version 4.0
 #requires -RunAsAdministrator
-Write-Host "PS Version and Admin Permissions passed" -ForegroundColor DarkBlue
+Write-Host "PS Version and Admin Permissions passed" -ForegroundColor DarkGreen
 
 # Pre-install warning/starting
 Write-Host "Starting install (check back here in about 10 minutes)..." -ForegroundColor DarkGreen
 Write-Host "Please do not touch or terminate this install (a macro is setup to do everything for you)" -ForegroundColor DarkRed
 
 #Install Chocolatey
-Write-Host "Installing Chocolatey..." -ForegroundColor Green
+Write-Host "Installing Chocolatey..." -ForegroundColor DarkGreen
 
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #Install git in order to use git bash
-Write-Host "Installing git..." -ForegroundColor Green
+Write-Host "Installing git..." -ForegroundColor DarkGreen
 choco install -y git
 refreshenv
 
