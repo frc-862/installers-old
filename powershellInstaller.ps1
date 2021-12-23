1f032e149b2a02dd8462655657fa167ee428ea48
Fritz Geib <fritz@standland.org>
2021-12-23 01:09:21 -0500

    PROG-82 use bright colors in powershell to match bash colors

--- powershellInstaller.ps1
+++ powershellInstaller.ps1
@@ -1,19 +1,18 @@
 #Check elevation and powershell ver first
 #requires -version 4.0
 #requires -RunAsAdministrator
-Write-Host "PS Version and Admin Permissions passed" -ForegroundColor DarkGreen
+Write-Host "PS Version and Admin Permissions passed" -ForegroundColor Green
 
 # Pre-install warning/starting
-Write-Host "Starting install (check back here in about 10 minutes)..." -ForegroundColor DarkGreen
-Write-Host "Please do not touch or terminate this install (a macro is setup to do everything for you)" -ForegroundColor DarkYellow
+Write-Host "Starting install (check back here in about 10 minutes)..." -ForegroundColor Green
+Write-Host "Please do not touch or terminate this install (a macro is setup to do everything for you)" -ForegroundColor Yellow
 
 #Install Chocolatey
-Write-Host "Installing Chocolatey..." -ForegroundColor DarkGreen
-
+Write-Host "Installing Chocolatey..." -ForegroundColor Green
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #Install git in order to use git bash
-Write-Host "Installing git..." -ForegroundColor DarkGreen
+Write-Host "Installing git..." -ForegroundColor Green
 choco install -y git
 refreshenv
 
