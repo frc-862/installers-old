a2d3e3ab2a66f631001b4c94374b4333961b7da0
Fritz Geib <fritz@standland.org>
2022-01-10 09:00:54 -0500

    PROG-94 add some todos

--- powershellInstaller.ps1
+++ powershellInstaller.ps1
@@ -1,14 +1,16 @@
 function ok { Write-Host "OK: $args" -ForegroundColor Green }
 function warn { Write-Host "WARNING: $args" -ForegroundColor Yellow }
-function showError { Write-Host "ERROR: $args" -ForegroundColor Red } #use showError instead of error to avoid issues when overriding built in cmdlets
+function showError { Write-Host "ERROR: $args" -ForegroundColor Red } #use showError instead of error to avoid issues when overriding built-in cmdlets
 
 # Run checks to make sure the installer can install
 #requires -version 4.0
 #requires -RunAsAdministrator
 
 #Check for enough disk space
-$freespace = (Get-CimInstance CIM_LogicalDisk -Filter "DeviceId='C:'").FreeSpace
+$freespace = (Get-CimInstance CIM_LogicalDisk -Filter "DeviceId='C:'").FreeSpace #TODO: check whatever drive the programs are installed to, not just C:/
 if (($freespace -lt 15gb)) {
+    #TODO: this disk space number is made up, need to make it more accurate
+    #TODO: make user able to override this error
     showError "You do not have enough disk space ('C:\') for this install! You need at least 15 gigabytes to run this installer."
     exit
 }
