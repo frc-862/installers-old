a8b824da39c55db0fdc88c816913e34bea4dac51
Fritz Geib <fritz@standland.org>
2022-01-15 10:16:24 -0500

    PROG-94 fix git and chocolatey installation

--- powershellInstaller.ps1
+++ powershellInstaller.ps1
@@ -22,13 +22,13 @@ ok "Starting install (check back here in about 10 minutes)..."
 warn "Please try not to touch the mouse while the installer is running. (a macro is setup to do everything for you)"
 
 #Install Chocolatey
-if (-not has "choco") {
+if (-not (has "choco")) {
     ok "Installing Chocolatey..."
     Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 }
 
 #Install git in order to use git bash
-if (-not has "git") {
+if (-not (has "git")) {
     ok "Installing git..."
     choco install -y git
     refreshenv
