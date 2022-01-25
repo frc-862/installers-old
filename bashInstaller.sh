fb558d398bcd7745b6fe2f1b32229f28438d59f8
Fritz Geib <fritz@standland.org>
2022-01-25 18:51:02 -0500

    PROG-149 that didnt work

--- bashInstaller.sh
+++ bashInstaller.sh
@@ -190,6 +190,7 @@ while [[ $# -gt 0 ]]; do
             ;;
         "--headless")
             INSTALL_WPILIB=false
+            INSTALL_NI=false
             INSTALL_PHOENIX=false
             INSTALL_REV=false
             SKIP_DEVWARN=true
